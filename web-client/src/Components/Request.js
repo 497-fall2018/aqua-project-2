280f27032869ee150492c5be7de7277fbcc223a8
James Xie <jamesxie@Jamess-MacBook-Pro.local>
2018-10-18 10:30:35 -0500

    added db + skeleton RequestARide

--- web-client/src/Components/Request.js
+++ web-client/src/Components/Request.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import '../styles/Main.css';
+
+import '../styles/Request.css';
 
 class Request extends Component {
   constructor(props) {
@@ -32,26 +33,51 @@ class Request extends Component {
     const { destination, time } = this.state;
 
     return (
-      <form className="main-request-ride" onSubmit={this.handleSubmit}>
-        <h2>Request a Ride</h2>
-        <select
-          className="main-request-ride-input"
-          defaultValue="Select a Destination"
-          name="destination"
-          onChange={this.handleChange}
-        >
-          <option value="ohare">O'Hare</option>
-          <option value="midway">Midway</option>
-        </select>
+      <form className="request-ride" onSubmit={this.handleSubmit}>
+      <img className="logo" src={require("../assets/images/FSLogo.svg")} />
+        <div className="request-ride-airport-container">
+          <div className="request-ride-category-header">Airport</div>
+          <select
+            className="request-ride-airport-input"
+            defaultValue="Select a Destination"
+            name="destination"
+            onChange={this.handleChange}>
+            <option value="ohare">O'Hare International Airport</option>
+            <option value="midway">Midway International Airport</option>
+          </select>
+        </div>
         {/* SELECT time, type="time" converts to military time thing */}
-        <input
-          className="main-request-ride-input"
-          type="time"
-          name="time"
-          value={time}
-          onChange={this.handleChange}
-        />
-        <button type="submit">Submit!</button>
+        <div className="request-ride-time-container">
+          <div className="request-ride-category-header">Time</div>
+          <input
+            className="request-ride-time-input"
+            type="time"
+            name="time"
+            value={time}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="request-ride-date-container">
+          <div className="request-ride-category-header">Date</div>
+          <input
+            className="request-ride-date-input"
+            type="date"
+            name="date"
+          />
+        </div>
+        <div className="request-ride-pickup-container">
+          <div className="request-ride-category-header">Pickup Location</div>
+          <select
+            className="request-ride-pickup-input"
+            defaultValue="Select a Pickup Location"
+            name="pickup">
+            <option value="anywhere">Anywhere</option>
+            <option value="northCampus">North Campus</option>
+            <option value="southCampus">South Campus</option>
+          </select>
+        </div>
+        
+        <button className="request-ride-submit" type="submit">Find a ride</button>
       </form>
     );
   }
