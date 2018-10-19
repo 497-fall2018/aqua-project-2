14cb06883408f948c62a0044d79a13577ea777a5
Zack Aslan <zackaslan@dhcp-10-105-60-63.wireless.northwestern.private>
2018-10-19 10:12:08 -0500

    updated

--- web-client/src/Components/UserProfile.js
+++ web-client/src/Components/UserProfile.js
@@ -17,7 +17,7 @@ class UserProfile extends Component {
             className="user-picture"
             src="https://i.pinimg.com/originals/f6/34/b9/f634b90aa5bdc4f7b6ed59bd553e97e5.jpg"
           />
-          Zack Aslan
+          Willie Wildcat
         </div>
 
         <PastRides />
