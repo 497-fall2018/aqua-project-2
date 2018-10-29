2c655a65b144f197ac63db5fa339b6bbc0fa11ac
szaslan <szaslan@gmail.com>
2018-10-29 10:06:04 -0500

    cleaned up code base

--- web-client/src/Components/EditProfile.js
+++ web-client/src/Components/EditProfile.js
@@ -1,78 +1,80 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/EditProfile.css';
 import saveIcon from '../assets/images/save.jpg';
 
-
-class EditProfile extends Component{
-    constructor(props) {
+class EditProfile extends Component {
+  constructor(props) {
     super(props); // access props
-    
+
     this.submitHandler = this.submitHandler.bind(this);
+  }
 
-    }
-    submitHandler(evt) {
-        evt.preventDefault();
-        this.props.handlerFromParant(true);
-    }
-    handleChange() {
-    }
-    render(){
-        return(
-            <div className="userprofile-container-form">
-                <div className="userprofile-form">
-                
-            <img
-              className="euser-picture"
-              src="https://i.pinimg.com/originals/f6/34/b9/f634b90aa5bdc4f7b6ed59bd553e97e5.jpg"
-            />
-            <div className = 'edit-info'>
-              <h4><strong>Edit Information</strong></h4>
-            </div>
-            <div className="inputName">
-            Name    
-            <input
-            name="Profile_name"
-            defaultValue="Willie Wildcat"
-            onChange={this.handleChange}
-            description="Profile_name"
-            type="text"
-            />
-            </div>
-            <div className="inputPhone">
-            Phone 
-            <input
-            name="Phone"
-            defaultValue="650-339-4731"
-            onChange={this.handleChange}
-            description="Phone"
-            type="text"
-            />
-            </div>
-            <div className="inputEmail">Email
-            <input
-            name="Email"
-            defaultValue="jamesxie2019@u.northwes..."
-            onChange={this.handleChange}
-            description="Email"
-            type="text"
-            />
-            </div>
-            <div className="inputBio">Bio   
-            <textarea
-            name="Bio"
-            defaultValue="CS student at Northwestern University."
-            onChange={this.handleChange}
-            description="Bio"
-            type="text"
-            />
-            </div>
-            <button className="save-profile" onClick={this.submitHandler}>
-              Save
-            </button>
-            </div>
+  submitHandler(evt) {
+    evt.preventDefault();
+    this.props.handlerFromParant(true);
+  }
 
+  handleChange() {}
+
+  render() {
+    return (
+      <div className="userprofile-container-form">
+        <div className="userprofile-form">
+          <img
+            className="euser-picture"
+            src="https://i.pinimg.com/originals/f6/34/b9/f634b90aa5bdc4f7b6ed59bd553e97e5.jpg"
+          />
+          <div className="edit-info">
+            <h4>
+              <strong>Edit Information</strong>
+            </h4>
           </div>
-        );
-    }
+          <div className="inputName">
+            Name
+            <input
+              name="Profile_name"
+              defaultValue="Willie Wildcat"
+              onChange={this.handleChange}
+              description="Profile_name"
+              type="text"
+            />
+          </div>
+          <div className="inputPhone">
+            Phone
+            <input
+              name="Phone"
+              defaultValue="650-339-4731"
+              onChange={this.handleChange}
+              description="Phone"
+              type="text"
+            />
+          </div>
+          <div className="inputEmail">
+            Email
+            <input
+              name="Email"
+              defaultValue="jamesxie2019@u.northwes..."
+              onChange={this.handleChange}
+              description="Email"
+              type="text"
+            />
+          </div>
+          <div className="inputBio">
+            Bio
+            <textarea
+              name="Bio"
+              defaultValue="CS student at Northwestern University."
+              onChange={this.handleChange}
+              description="Bio"
+              type="text"
+            />
+          </div>
+          <button className="save-profile" onClick={this.submitHandler}>
+            Save
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-export default EditProfile
+export default EditProfile;
--- web-client/src/Components/UserProfile.js
+++ web-client/src/Components/UserProfile.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import '../styles/UserProfile.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import editIcon from '../assets/images/edit.jpg';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit} from '@fortawesome/free-solid-svg-icons'
-library.add(faEdit)
+
+library.add(faEdit);
 
 class UserProfile extends Component {
   constructor(props) {
     super(props); // access props
 
-    
     this.submitHandler = this.submitHandler.bind(this);
     this.state = {
-      Username:'Willie Wildcat',
-      Phone:'650-339-4731',
-      Email:'jamesxie2019@u.north...',
-      Bio:'CS student at Northwestern University.',
+      Username: 'Willie Wildcat',
+      Phone: '650-339-4731',
+      Email: 'jamesxie2019@u.north...',
+      Bio: 'CS student at Northwestern University.',
     };
-    }
-    submitHandler(evt) {
-        evt.preventDefault();
-        this.props.handlerFromParant(false);
-    }
+  }
+
+  submitHandler(evt) {
+    evt.preventDefault();
+    this.props.handlerFromParant(false);
+  }
+
   render() {
     return (
       <div className="userprofile-container">
@@ -30,25 +32,36 @@ class UserProfile extends Component {
           <img
             className="user-picture"
             src="https://i.pinimg.com/originals/f6/34/b9/f634b90aa5bdc4f7b6ed59bd553e97e5.jpg"
-          />         
-          <div className="username"><h4><strong>{this.state.Username}</strong></h4></div>
+          />
+          <div className="username">
+            <h4>
+              <strong>{this.state.Username}</strong>
+            </h4>
+          </div>
           <button className="edit-profile" onClick={this.submitHandler}>
-              <FontAwesomeIcon icon= "edit"/>
+            <FontAwesomeIcon icon="edit" />
           </button>
-       </div>
-        <div className="contact-details">
-          <header><h4><strong>Contact</strong></h4></header>
-        <div className="phone-details"><strong>Phone</strong>: {this.state.Phone}</div>
-        
-        <div className="email-details"> <strong>Email</strong>: {this.state.Email}</div>
         </div>
-        <div className="bio-details">
-        <h4><strong>Bio</strong></h4> 
-          <div className = "bio-info">
-            {this.state.Bio}
+        <div className="contact-details">
+          <header>
+            <h4>
+              <strong>Contact</strong>
+            </h4>
+          </header>
+          <div className="phone-details">
+            <strong>Phone</strong>: {this.state.Phone}
           </div>
 
-          
+          <div className="email-details">
+            {' '}
+            <strong>Email</strong>: {this.state.Email}
+          </div>
+        </div>
+        <div className="bio-details">
+          <h4>
+            <strong>Bio</strong>
+          </h4>
+          <div className="bio-info">{this.state.Bio}</div>
         </div>
       </div>
     );
